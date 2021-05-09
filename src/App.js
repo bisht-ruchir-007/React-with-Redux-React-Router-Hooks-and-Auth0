5644cdc147abe481a2dfcd4e4821c3747678bb2a
Ruchir Bisht <ruchircr7@gmail.com>
2021-05-09 12:14:49 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	userName = 'RUCHIR BISHT';
+	render() {
+		//userName = 'RUCHIR BISHT'; won't work
+		// const / let / var userName will work
+		const date = new Date();
+		const data = {
+			key1: 'Some data'
+		};
+
+		return (
+			<div className='container'>
+				<Jumbotron>
+					<h1>
+						Welcome {this.userName}
+						{/* {console.log(this.props)} */}
+					</h1>
+					{/* Displaying Objects in JSX */}
+					{/* {React.createElement('h2', { className: 'app' }, var1.key1)} */}
+					<h2>{data.key1}</h2>
+
+					{/* Use Date and Time  */}
+					<p className='lead'>{date.toDateString()}</p>
+					<Button className='btn btn-success'>Submit</Button>
+				</Jumbotron>
+			</div>
+		);
+	}
 }
+
+const AppInstance = new App();
+
+console.log(AppInstance.userName);
 
 export default App;
