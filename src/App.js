5701ed19a7711de78711a126fcea9919a5d72fe1
Ruchir Bisht <ruchircr7@gmail.com>
2021-05-09 12:53:22 +0530

    Props in functional components

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import { Jumbotron, Button } from 'react-bootstrap';
 import Component1 from './components/functional/Component1';
 
 class App extends Component {
-	userName = 'RUCHIR BISHT';
+	//userName = 'RUCHIR BISHT';
 	render() {
 		//userName = 'RUCHIR BISHT'; won't work
 		// const / let / var userName will work
 		const date = new Date();
-		const data = {
-			key1: 'Some data'
+		const userData = {
+			username: 'ruchircr7',
+			name: 'Ruchir Bisht',
+			age: 21
 		};
 
 		return (
 			<div className='container'>
+				<span className='badge bg-success text-light'>{date.toDateString()}</span>
+
 				<Jumbotron>
-					<h1>
-						Welcome {this.userName}
+					<h1 className='text-primary'>
+						Welcome {userData.username}
 						{/* {console.log(this.props)} */}
 					</h1>
-					{/* Displaying Objects in JSX */}
-					{/* {React.createElement('h2', { className: 'app' }, var1.key1)} */}
-					<h2>{data.key1}</h2>
-					<Component1 />
-					{/* Use Date and Time  */}
-					<p className='lead'>{date.toDateString()}</p>
-					<Button className='btn btn-success'>Submit</Button>
 				</Jumbotron>
+
+				{/* Displaying Objects in JSX */}
+				{/* {React.createElement('h2', { className: 'app' }, var1.key1)} */}
+				<Component1 name={userData.name} age={userData.age} />
+				{/* Use Date and Time  */}
 			</div>
 		);
 	}
--- src/components/functional/Component1.js
+++ src/components/functional/Component1.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Component1 = (props) => {
-	return <div>Component-1</div>;
+const Component1 = ({ name, age }) => {
+	return (
+		<div>
+			<h4 className='text-info'>Personal Details</h4>
+			<h5>Name : {name}</h5>
+			<h5>Age : {age}</h5>
+		</div>
+	);
 };
 
 export default Component1;
